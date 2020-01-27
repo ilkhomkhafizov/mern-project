7d41e0a6bcd67916e51c714fd001bf728b3bea0a
ilkhomkhafizov <ilhomhafizov6@gmail.ru>
2020-01-27 05:47:07 +0300

    final changes for deploy

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const config = require("config");
+const path = require("path");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -11,6 +12,13 @@ app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/link", require("./routes/link.routes"));
 
 app.use("/t", require("./routes/redirect.routes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "frontend", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 const PORT = config.get("port") || 5000;
 
